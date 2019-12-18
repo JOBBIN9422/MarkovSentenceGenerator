ce7d120051387fc1631166623c5b30c204c367ef
James Aubin <32369412+JOBBIN9422@users.noreply.github.com>
2019-12-18 15:34:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Markov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SentenceGenerator markov = new SentenceGenerator("white-waterfall.txt");
+
+            for (;;)
+            {
+                string test = markov.GenerateSentence();
+                Console.WriteLine(test);
+                Console.ReadKey();
+            }
+        }
+    }
+}
